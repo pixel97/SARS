0ecbca0ba5fdc45fbe32bcaf992f6493f9770179
kurumaddali-s <57618935+kurumaddali-s@users.noreply.github.com>
2022-12-11 21:32:57 -0500

    Add files via upload

---
+++ server/config/roles.js
@@ -0,0 +1,26 @@
+const AccessControl = require('accesscontrol');
+
+const allRights = {
+    'create:any':['*'],
+    'read:any':['*'],
+    'update:any':['*'],
+    'delete:any':['*']
+}
+
+let grantsObject = {
+    admin:{
+        profile:allRights,
+        brand:allRights
+    },
+    user:{
+        profile:{
+            'read:own':['*','!password','!_id'],
+            'update:own':['*']
+        },
+        brand:{ 'read:any':['*'] }
+    }
+}
+
+const roles = new AccessControl(grantsObject)
+
+module.exports = { roles }
